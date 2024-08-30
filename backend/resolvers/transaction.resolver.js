7a1a9b7d964d77be0cc61e605dd191723eab8cf1
Vidyaranya-Gavai-TTPL <vidyaranya.gavai@tekditechnologies.com>
2024-08-30 09:42:42 +0530

    Transaction Resolvers Completed

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,72 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    transactions: async (_parent, _input, context) => {
+      try {
+        if (!context.getUser()) {
+          throw new Error("Unauthorized");
+        }
+        const userId = await context.getUser()._id;
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        console.log(`Error getting transactions: ${error}`);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+
+    transaction: async (_parent, { transactionId }, _context) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        console.log(`Error getting transaction: ${error}`);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+  },
+  Mutation: {
+    createTransaction: async (_parent, { input }, context) => {
+      try {
+        const newTransaction = new Transaction({
+          ...input,
+          userId: await context.getUser()._id,
+        });
+        await newTransaction.save();
+        return newTransaction;
+      } catch (error) {
+        console.log(`Error creating transactions: ${error}`);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+
+    updateTransaction: async (_parent, { input }, _context) => {
+      try {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+          input.transactionId,
+          input,
+          { new: true }
+        );
+        return updatedTransaction;
+      } catch (error) {
+        console.log(`Error updating transactions: ${error}`);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+
+    deleteTransaction: async (_parent, { transactionId }, _context) => {
+      try {
+        const deletedTransaction = await Transaction.findByIdAndDelete(
+          transactionId
+        );
+        return deletedTransaction;
+      } catch (error) {
+        console.log(`Error updating transactions: ${error}`);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+  },
 };
 
 export default transactionResolver;
